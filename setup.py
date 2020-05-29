16f65cbb87b03c5c3ed91b54b881e8a3be43e53d
Otávio Vicente Ribeiro <otavio.r.filho@gmail.com>
2020-05-29 01:21:50 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for the NASA Space Apps Hackaton',
+    author='Diogo Hideki, Otávio Ribeiro, Paula Moraes, Pedro Marcelino, Rosana Ribeiro',
+    license='MIT',
+)
