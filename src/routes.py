4ad2e108c4265ba5848ecbf77cdf71a3f9f90187
pedronovaes <pedronovaesmelo@gmail.com>
2020-05-31 20:00:45 -0300

    Adding images and updating application structure

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,23 @@
 from src import app
-from flask import render_template, url_for
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
